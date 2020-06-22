55193179385f827a235742edccb8aa8f58412df1
Adrien Joseph Dion <adion81@gmail.com>
2020-06-22 20:22:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,11 +16,19 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
+  else if(age > 19){
+    return "You are a grownup"
+  }
+  else{
+    return "You are a kid"
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
